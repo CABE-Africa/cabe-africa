7e115d921dd0d6e0984884947342458e49d39d66
Azenga Kevin <azenga.kevin7@gmail.com>
2022-05-30 16:46:44 +0300

    WIP: Added fontawesome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,13 +11,52 @@
 </head>
 
 <body class="antialiased text-center">
-    <h1 class="text-primary">{{ config("app.name") }}</h1>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex omnis voluptatibus iste, deserunt laboriosam sed
-        doloribus placeat laudantium nesciunt asperiores vel deleniti adipisci aut perferendis inventore ea repudiandae
-        recusandae unde, doloremque nihil! Dolores deleniti quos nemo et, natus rem laboriosam molestias saepe similique
-        earum! Maxime iste nisi doloremque aperiam voluptatum cupiditate laudantium eius explicabo et aliquam ducimus
-        adipisci repudiandae, esse hic optio harum quas!</p>
-        
+    <header class="shadow-sm">
+        <div class="bg-primary">
+            <div class="container">
+                <div class="d-flex gap-3 justify-content-md-between">
+                    <div class="d-flex gap-3 p-2 p-md-3">
+                        <a href="#" class="text-white text-decoration-none d-inline-flex gap-2 align-items-center">
+                            <i class="fa-solid fa-phone"></i>
+                            <span class="d-none d-sm-inline">+254714097380</span>
+                        </a>
+                        <a href="#" class="text-white text-decoration-none d-inline-flex gap-2 align-items-center">
+                            <i class="fa-solid fa-envelope"></i>
+                            <span class="d-none d-sm-inline">info@cabe-africa.org</span>
+                        </a>
+                    </div>
+                    <div class="d-flex">
+                    <a href="#" class="text-white text-decoration-none d-inline-flex gap-2 align-items-center">
+                            <i class="fa-solid fa-graduation-cap"></i>
+                            <span class="d-none d-sm-inline">Careers</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <nav class="navbar navbar-expand-md navbar-light">
+            <div class="container">
+                <a href="#" class="navbar-brand">
+                    <img height="48" src="{{ asset('images/logo.png') }}" alt="CABE Africa">
+                </a>
+                <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#global-navigation-menu">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="global-navigation-menu">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item"><a href="#" class="nav-link">Home</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Contact Us</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Who We Are</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">What We Do</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">News and Publications</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Resources</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Blogs</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+
     <script src="{{ mix("js/app.js") }}"></script>
 </body>
 
