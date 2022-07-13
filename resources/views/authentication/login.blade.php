b4285dd6e4ae4ea6cc23d2ca0c618db48a3e0cf0
Azenga Kevin <azenga.kevin7@gmail.com>
2022-07-13 08:14:19 +0300

    WIP: A bit more on bootstrap CSS utilities and login register pages enhancement

--- resources/views/authentication/login.blade.php
+++ resources/views/authentication/login.blade.php
@@ -4,9 +4,21 @@
 
 @section('body')
 <div class="row g-0 min-vh-100">
-    <div class="col-md-6 bg-primary"></div>
+    <div class="col-md-6 bg-primary bg-gradient p-3 p-sm-4 p-md-5">
+        <div class="d-flex flex-column gap-3 align-items-start">
+            <a href="{{ route('welcome') }}" class="btn btn-light d-inline-flex gap-2 align-items-center rounded-pill">
+                <i class="fa-solid fa-arrow-left-long"></i>
+                <span>Home</span>
+            </a>
+            <a href="{{ route('welcome') }}" class="display-6 text-decoration-none text-white">Center for Africa Bio-Entrepreneurship</a>
+
+            <p class="lead text-white-50">All you need to know agriculture and agripreneurship in Africa in one place</p>
+
+            <img class="w-100 img-thumbnail d-none d-md-block" src="{{ asset('images/it-in-the-field-2.jpg') }}" alt="IT in the Field">
+        </div>
+    </div>
     <div class="col-md-6 d-flex justify-content-center align-items-center">
-        <div class="w-50">
+        <div class="w-75 w-md-50">
             <h1>Sign In</h1>
             <form action="{{ route('login') }}" method="POST">
                 @csrf
